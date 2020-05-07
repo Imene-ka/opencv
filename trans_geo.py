f63770fdefc2dd0f372f285b4b17db1339bc0b9b
karboua <karboua.imene@gmail.com>
2020-05-07 20:33:07 +0100

    exo2

--- trans_geo.py
+++ trans_geo.py
@@ -1,8 +1,16 @@
 import cv2
 import sys
 
-img = cv2.imread("/home/client/Images/a2.png")
-if img.size == 0 :
+src = cv2.imread("/home/client/Images/a2.png")
+if src.size == 0 :
     sys.exit("Error: the image has not been correctly loaded.")
-
+des=cv2.Mat()
+desize=cv2.Size(src.rows,src.cols)
+center=cv2.point(src.cols/2,src.rows /2)
+m=cv2.getRotationMatrix2D(center,45,1)
+cv2.warpAffine(src,des,m,desize,cv2.INTER_LINEAR,cv2.BORDER_CONSTANT,cv2.Scalar())
+cv.imshow("new windows",des)
+src.delete()
+des.delete()
+m.delete()
 
