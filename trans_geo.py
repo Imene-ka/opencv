437c22e3c9d0c2e7a7f3618b3ce6c47708182d9e
karboua <karboua.imene@gmail.com>
2020-05-08 20:19:48 +0100

    version final

--- trans_geo.py
+++ trans_geo.py
@@ -1,16 +1,29 @@
 import cv2
 import sys
+import math
+from math import floor
+import numpy as np
+
+
+def fonction(img,d):
+  p=img.shape
+  l=p[0]
+  c=p[1]
+  i=0
+  result = np.zeros((l,c, 3), np.uint8)
+  while i<l :
+      j=0
+      while j<c :
+          (r,g,b) = img[i,j]
+          i_n=floor(i*math.cos(d)-j*math.sin(d))
+          j_n=floor(j*math.cos(d)+i*math.sin(d))
+          result[i_n,j_n]= (r,g,b)
+          j=j+1
+      i=i+1
+  cv2.imshow("new image",result)
+
 
 src = cv2.imread("/home/client/Images/a2.png")
 if src.size == 0 :
     sys.exit("Error: the image has not been correctly loaded.")
-des=cv2.Mat()
-desize=cv2.Size(src.rows,src.cols)
-center=cv2.point(src.cols/2,src.rows /2)
-m=cv2.getRotationMatrix2D(center,45,1)
-cv2.warpAffine(src,des,m,desize,cv2.INTER_LINEAR,cv2.BORDER_CONSTANT,cv2.Scalar())
-cv.imshow("new windows",des)
-src.delete()
-des.delete()
-m.delete()
-
+fonction(src,40)
